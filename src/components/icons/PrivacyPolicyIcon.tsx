141158ba8d632be0aca4beff2e91b64da1f73df2
Siva-S92 <sivagraphics4fashion@gmail.com>
2025-08-01 14:57:19 +0530

    updated custom icon component

--- src/components/icons/PrivacyPolicyIcon.tsx
+++ src/components/icons/PrivacyPolicyIcon.tsx
@@ -1,13 +1,9 @@
-// components/PrivacyPolicyIcon.tsx
-
 'use client';
 
-// No need to import PlayerInstance
 import { Player } from '@lordicon/react';
 // Import the 'ComponentRef' type from React
 import { useRef, useEffect, ComponentRef } from 'react';
 
-// Make sure the JSON file is in the same directory or use a correct relative path
 import iconData from 'public/assets/icons/privacy-policy-icon.json';
 
 // This is the correct type for the Player component's ref
@@ -19,7 +15,6 @@ const PrivacyPolicyIcon = () => {
 
   useEffect(() => {
     const playAnimation = () => {
-      // TypeScript is now happy because it can infer the methods from the component type
       playerRef.current?.playFromBeginning();
     };
 
